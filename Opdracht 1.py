6455cc2bccc5c397913883c8a4eb6f2902637f2f
KeithRhandell-1056108 <1056108@hr.nl>
2022-09-14 12:30:58 +0200

    Added .py files - opdrachten

---
+++ Opdracht 1.py
@@ -0,0 +1,49 @@
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2//3)
+print(2**3)
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+print(3//2)
+print(3**2)
+
+print(23.95 * 32.9)
+print(23.95 * 32.9 + 6)
+print((23.95 * 32.9) + 6)
+print(23.95 * (32.9 + 6))
+print(23.95 / 32.9)
+print(23.95 / 32.9 + 6)
+print((23.95 / 32.9) + 6)
+print(23.95 / (32.9 + 6))
+
+name = "John Doe"
+print(name)
+
+street = "Neverland Lane"
+print(street)
+
+items = 5
+print(items)
+
+price = 19.95
+print(price)
+
+students = 28
+teachers = 3
+seats = students + teachers
+print(seats)
+
+correct = students > teachers
+print(correct)
+
+incorrect = students > seats
+print(incorrect)
+
+line_character = '-'
+line = line_character * 20
+print(line)
+
---
+++ Opdracht 2.py
@@ -0,0 +1,47 @@
+people = 7861304740
+print(people)
+people = 7_861_304_740
+print(people)
+
+meals = 3
+people_eat = people * meals
+print(people_eat)
+
+days = 365
+meals_per_year = people_eat * days
+print(meals_per_year)
+
+type(meals_per_year)
+
+#Float
+
+# Ethernet capacity
+ethernet_speed_mbps = 1000
+efficiency = 0.7
+maximum_capacity = ethernet_speed_mbps * efficiency
+print(maximum_capacity)
+
+# Print capacity used on the ethernet
+download_speed_average = 96.25
+usage = ethernet_speed_mbps / download_speed_average
+print(usage)
+
+# Speed of light in m/s
+speed_of_light = 299_792_458
+
+# Distance Rotterdam / New York in km
+distance_Rotterdam_NewYork = 5_862.03
+time_to_reach_NYC = (distance_Rotterdam_NewYork * 1000) / speed_of_light
+print(time_to_reach_NYC)
+
+# Welk datatype is het?
+type(time_to_reach_NYC)
+
+#String
+
+ship = 'Titanic'
+print(ship)
+
+sentence = "He doesn’t think it’s a good idea to spend all his money on videogames."
+print(sentence)
+
